02663088e581aef45f7c453e00fad81d8acc1bf9
zhaoyanwen <zhaoyanwen@worken.cn>
2020-01-08 10:34:12 +0800

    icon

--- LibInject/resource.h
+++ LibInject/resource.h
@@ -3,6 +3,7 @@
 // π© LibInject.rc  π”√
 //
 #define IDD_MAIN_DLG                    101
+#define IDI_ICON1                       103
 #define ID_INJECT_DLL                   1001
 #define ID_UNINJECT_DLL                 1002
 #define IDC_PROC_NAME                   1003
@@ -12,7 +13,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
